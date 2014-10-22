632776e532471ca506c38bc20f9e8f7d144c1140
stephane71 <stephanemaire71@gmail.com>
2014-10-22 13:27:16 +0200

    set index page

--- app/scripts/routes/IndexRoute.js
+++ app/scripts/routes/IndexRoute.js
@@ -1,5 +1,5 @@
 Ptn2015.IndexRoute = Ember.Route.extend({
-    model: function () {
-		return this.store.find('startup');
-    }
+/*    model: function () {*/
+		//return this.store.find('startup');
+    /*}*/
 });
--- app/scripts/views/IndexView.js
+++ app/scripts/views/IndexView.js
@@ -1,25 +1,5 @@
 Ptn2015.IndexView = Ember.View.extend({
 	didInsertElement: function(){
-		var self = this;
-		this.updateSearchList();
-		$('#startup-search').on('select2-selecting', function(e){
-			self.get('controller').onStartupSelection(e.object.text);
-		});
+
 	},
-
-	updateSearchList: function(){
-		var list = this.get('controller.list');
-		$('#startup-search').select2({
-			data: list
-		});
-	}.observes('controller.list'),
-
-	click: function(e){
-		if(e.target.id === 'all'){
-			$("#category_menu a").each(function(i, e){
-				$(e).removeClass('active');
-			});
-			$(e.target).addClass('active');
-		}
-	}
 });
