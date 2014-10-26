389477cd234ad1cf4ff82608884d80bbc075e871
Stephane Maire <stephanemaire71@gmail.com>
2014-10-26 12:27:03 +0100

    set facebook api loading in index view

--- app/scripts/views/IndexView.js
+++ app/scripts/views/IndexView.js
@@ -1,5 +1,22 @@
 Ptn2015.IndexView = Ember.View.extend({
 	didInsertElement: function(){
-
+		this.loadFacebookButton();
 	},
+
+	loadFacebookButton: function() {
+		var d = document,
+			s = 'script',
+			id = 'facebook-jssdk';
+
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {
+			FB.XFBML.parse(d.getElementById(id));
+			return;
+		}
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=1477818409128838&version=v2.0";
+		fjs.parentNode.insertBefore(js, fjs);
+	}
+
 });
